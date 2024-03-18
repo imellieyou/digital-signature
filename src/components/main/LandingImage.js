896c87385178d9fb2679ec514301a32193f13efe
bonnie <rilee@jinhakapply.com>
2024-03-18 18:01:19 +0900

    style: 랜딩이미지 컴포넌트 기본 UI

---
+++ src/components/main/LandingImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const LandingImage = () => {
+  return <LandingWrap>LandingImage</LandingWrap>;
+};
+
+const LandingWrap = styled.div`
+  width: 67vw;
+`;
+
+export default LandingImage;
