db5909eda748ff62db0cd63e077ed4437826864f
bonnie <rilee@jinhakapply.com>
2024-03-18 16:45:00 +0900

    feat: 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import styled from "styled-components";
-import Paper from "./Paper";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Contract from "./pages/Contract";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <AppWrap>
-      <Header>전자서명</Header>
-      <Body>
-        호오
-        <Paper />
-      </Body>
-      <Footer>Copyright © 2024 imellieyou. All rights reserved.</Footer>
-    </AppWrap>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/contract" element={<Contract />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-const AppWrap = styled.div`
-  background-color: #4682b4;
-  color: #eee;
-  width: 40vw;
-  height: 100vh;
-  margin: auto;
-  padding: 20px 30px;
-  box-sizing: border-box;
-`;
-
-const Header = styled.header`
-  font-size: 32px;
-  font-weight: 600;
-  letter-spacing: -0.04em;
-  margin-bottom: 20px;
-`;
-
-const Body = styled.body`
-  width: 100%;
-  height: 85vh;
-  background-color: #fff;
-`;
-const Footer = styled.footer`
-  text-align: center;
-  margin-top: 25px;
-  font-size: 12px;
-  color: #eee;
-`;
 
 export default App;
