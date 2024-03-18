90b89238645cd8fd18458bcac5bd59e7f8f8025c
bonnie <rilee@jinhakapply.com>
2024-03-18 18:00:56 +0900

    style: 키패드 컴포넌트 기본 UI

---
+++ src/components/main/Keypad.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+import styled from "styled-components";
+
+const Keypad = () => {
+  useEffect(() => {}, []);
+
+  return <KeypadWrap>Keypad</KeypadWrap>;
+};
+
+const KeypadWrap = styled.div`
+  width: 100%;
+`;
+
+export default Keypad;
